58c7f6d79b0b14dd576c7e4491a7a8543ef70dc2
yoelbin <yoelbin@gmail.com>
2020-11-03 16:22:59 +0200

    package json update with script and homepage

---
+++ build/precache-manifest.389fea86d6eccbb4b98ab7497b755ac4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3fb23a598fd780489fa710342de059",
+    "url": "/Notes/index.html"
+  },
+  {
+    "revision": "15082ecdcf964d71e394",
+    "url": "/Notes/static/js/2.c766c032.chunk.js"
+  },
+  {
+    "revision": "323bec0f07d73542da6f",
+    "url": "/Notes/static/js/main.a16fe467.chunk.js"
+  },
+  {
+    "revision": "177054f91f78be324b15",
+    "url": "/Notes/static/js/runtime-main.ca9b7305.js"
+  }
+]);
