a423db0132e70d01e012deea4130dc0f4fc289cb
yoelbin <yoelbin@gmail.com>
2022-01-10 12:42:04 +0200

    Updates

---
+++ precache-manifest.05c60efe2dd7cbeb7169e09fd2c3ebd6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a93f843b3cf8b9eb0bfff5b08caab5d3",
+    "url": "/Notes/index.html"
+  },
+  {
+    "revision": "15082ecdcf964d71e394",
+    "url": "/Notes/static/js/2.c766c032.chunk.js"
+  },
+  {
+    "revision": "323bec0f07d73542da6f",
+    "url": "/Notes/static/js/main.a16fe467.chunk.js"
+  },
+  {
+    "revision": "177054f91f78be324b15",
+    "url": "/Notes/static/js/runtime-main.ca9b7305.js"
+  }
+]);
